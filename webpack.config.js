1e081ad791b2b5552386f70a9ff69d34dd999f3e
Davis Goff <davis.goff@ucf.edu>
2019-07-19 12:04:18 -0400

    Update webpack and package.json, remove unneeded elements

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,41 @@
 const fs = require('fs')
 const path = require('path')
-const marked = require('meta-marked')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const srcPath = path.join(__dirname, 'src') + path.sep
 const outputPath = path.join(__dirname, 'build') + path.sep
 const widgetWebpack = require('materia-widget-development-kit/webpack-widget')
 
-const entries = widgetWebpack.getDefaultEntries()
 const copy = widgetWebpack.getDefaultCopyList()
 
-entries['guides/guideStyles.css'] = [srcPath+'_helper-docs/guideStyles.scss']
+const entries = {
+	'player.js': [
+			path.join(srcPath, 'player.coffee')
+	],
+	'creator.js': [
+			path.join(srcPath, 'creator.coffee')
+	],
+	'player.css': [
+			path.join(srcPath, 'player.html'),
+			path.join(srcPath, 'player.scss')
+	],
+	'creator.css': [
+			path.join(srcPath, 'creator.html'),
+			path.join(srcPath, 'creator.scss')
+	],
+	'guides/player.temp.html': [
+			path.join(srcPath, '_guides', 'player.md')
+	],
+	'guides/creator.temp.html': [
+			path.join(srcPath, '_guides', 'creator.md')
+	]
+}
 
 const customCopy = copy.concat([
 	{
-		from: `${srcPath}/_helper-docs/assets`,
-		to: `${outputPath}/guides/assets`,
+		from: path.join(srcPath, '_guides', 'assets'),
+		to: path.join(outputPath, 'guides', 'assets'),
 		toType: 'dir'
 	}
 ])
-
 
 //options for the build
 const options = {
@@ -26,24 +43,6 @@ const options = {
 	entries: entries
 }
 
-const generateHelperPlugin = name => {
-	const file = fs.readFileSync(path.join(__dirname, 'src', '_helper-docs', name+'.md'), 'utf8')
-	const content = marked(file)
-
-	return new HtmlWebpackPlugin({
-		template: path.join(__dirname, 'src', '_helper-docs', 'helperTemplate'),
-		filename: path.join(outputPath, 'guides', name+'.html'),
-		title: name.charAt(0).toUpperCase() + name.slice(1),
-		chunks:['guides'],
-		content: content.html
-	})
-}
-
 let buildConfig = widgetWebpack.getLegacyWidgetBuildConfig(options)
 
-buildConfig.plugins.unshift(generateHelperPlugin('creator'))
-buildConfig.plugins.unshift(generateHelperPlugin('player'))
-
 module.exports = buildConfig
-
-//module.exports = require('materia-widget-development-kit/webpack-widget').getLegacyWidgetBuildConfig()
